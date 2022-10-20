8014ba415d66b46b6d65731b4cca8543e2ec1282
Artos Web <teodor.at.tod@gmail.com>
2022-10-20 14:34:13 +0300

    add navbar

---
+++ cats/components/Nav/Nav.js
@@ -0,0 +1,12 @@
+
+function Nav() {
+    return (
+        <nav class="navbar bg-light">
+            <div class="container-fluid">
+                <span class="navbar-brand mb-0 h1">Navbar</span>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav
