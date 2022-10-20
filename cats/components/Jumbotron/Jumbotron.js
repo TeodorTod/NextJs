862009e92d0939f896b3dc0dff7b844fbb570c7d
Artos Web <teodor.at.tod@gmail.com>
2022-10-20 16:09:05 +0300

    add Jumbotrone component

---
+++ cats/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,14 @@
+
+export default function Jumbotron() {
+    return (
+        <div class="jumbotron jumbotron-fluid mt-5">
+            <div class="container">
+                <h1 class="display-4">Cats World</h1>
+                <p class="lead">
+                    Find your new best friend today. Adobt a wonderful cat.
+                </p>
+                <button type="button" class="btn btn-primary">Start looking</button>
+            </div>
+        </div>
+    )
+}
