3d3a184046e3604e1c834b25c9195f375118bb17
Artos Web <teodor.at.tod@gmail.com>
2022-11-08 23:53:37 +0200

    style nav

--- social/components/Nav.js
+++ social/components/Nav.js
@@ -1,7 +1,19 @@
+import Link from "next/link";
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <nav className="flex justify-between items-center py-10">
+      <Link href="/">
+        <button className="text-lg font-medium">Creative Minds</button>
+      </Link>
+      <ul className="flex items-center gap-10">
+        <Link legacyBehavior href={'/auth/login'}>
+          <a className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">
+            Join Now
+          </a>
+        </Link>
+      </ul>
+    </nav>
   )
 }
 
