621c2e9f8bc05b68164085f27bc6bb3cbe79e01e
Artos Web <teodor.at.tod@gmail.com>
2022-11-09 13:19:31 +0200

    add link to dashboard and post pages

--- social/components/Nav.js
+++ social/components/Nav.js
@@ -1,20 +1,36 @@
 import Link from "next/link";
+import { auth } from "../utils/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
-function Nav() {
+export default function Nav() {
+  const [user, loading] = useAuthState(auth);
+
   return (
     <nav className="flex justify-between items-center py-10">
-      <Link href="/">
+      <Link legacyBehavior href="/">
         <button className="text-lg font-medium">Creative Minds</button>
       </Link>
       <ul className="flex items-center gap-10">
-        <Link legacyBehavior href={'/auth/login'}>
-          <a className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">
-            Join Now
-          </a>
-        </Link>
+        {!user && (
+          <Link legacyBehavior href={"/auth/login"}>
+            <a className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">
+              Join Now
+            </a>
+          </Link>
+        )}
+        {user && (
+          <div className="flex items-center gap-6">
+            <Link legacyBehavior href="/post">
+              <button className="font-medium bg-cyan-500 text-white py-2 px-4 rounded-mg textx-sm">
+                Post
+              </button>
+            </Link>
+            <Link legacyBehavior href="/dashboard">
+              <h1>Dashboard</h1>
+            </Link>
+          </div>
+        )}
       </ul>
     </nav>
-  )
+  );
 }
-
-export default Nav
