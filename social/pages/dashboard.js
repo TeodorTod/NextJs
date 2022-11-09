84fef9c181f570d4134d910e8d2f7de09e406c5a
Artos Web <teodor.at.tod@gmail.com>
2022-11-09 13:35:49 +0200

    redirect to dashboard to logged out user

--- social/pages/dashboard.js
+++ social/pages/dashboard.js
@@ -1,6 +1,24 @@
-import { auth } from "../utils/firebase"
+import { auth } from "../utils/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 function Dashboard() {
+    const route = useRouter();
+    const [user, loading] = useAuthState(auth);
+    //See if user is logged
+    const getData = async () => {
+        if (loading) return;
+        if (!user) return route.push('/auth/login');
+    }
+
+    //Get users data
+
+    useEffect(() => {
+        getData();
+    }, [user, loading])
+
+
     return (
         <div>
             <h1>Your posts</h1>
