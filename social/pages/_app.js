e9a61a761d571038d6dea98bf663e035ea4dda4c
Artos Web <teodor.at.tod@gmail.com>
2022-11-10 21:33:05 +0200

    add message component

--- social/pages/_app.js
+++ social/pages/_app.js
@@ -1,9 +1,12 @@
 import '../styles/globals.css';
 import Layout from '../components/layout';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <ToastContainer limit={1}/>
       <Component {...pageProps} />
     </Layout>
   )
--- social/pages/post.js
+++ social/pages/post.js
@@ -1,29 +1,92 @@
 import { auth, db } from "../utils/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
+import Router, { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import {
+    addDoc,
+    collection,
+    doc,
+    serverTimestamp,
+    updateDoc,
+} from "firebase/firestore";
+import { toast } from "react-toastify";
 
-function Post() {
+export default function Post() {
     //Form state
-    const [post, setPost] = useState({ description: "" })
+    const [post, setPost] = useState({ description: "" });
+    const [user, loading] = useAuthState(auth);
+    const route = useRouter();
+    const routeData = route.query;
 
-    //Submit post
+    //Submit Post
     const submitPost = async (e) => {
         e.preventDefault();
+        //Run checks for description
+        if (!post.description) {
+            toast.error("Description Field empty 😅", {
+                position: toast.POSITION.TOP_CENTER,
+                autoClose: 1500,
+            });
+            return;
+        }
+        if (post.description.length > 300) {
+            toast.error("Description too long 😅", {
+                position: toast.POSITION.TOP_CENTER,
+                autoClose: 1500,
+            });
+            return;
+        }
 
-    }
+        if (post?.hasOwnProperty("id")) {
+            const docRef = doc(db, "posts", post.id);
+            const updatedPost = { ...post, timestamp: serverTimestamp() };
+            await updateDoc(docRef, updatedPost);
+            return route.push("/");
+        } else {
+            //Make a new post
+            const collectionRef = collection(db, "posts");
+            await addDoc(collectionRef, {
+                ...post,
+                timestamp: serverTimestamp(),
+                user: user.uid,
+                avatar: user.photoURL,
+                username: user.displayName,
+            });
+            setPost({ description: "" });
+            toast.success("Post has been made 🚀", {
+                position: toast.POSITION.TOP_CENTER,
+                autoClose: 1500,
+            });
+            return route.push("/");
+        }
+    };
+
+    //Check our user
+    const checkUser = async () => {
+        if (loading) return;
+        if (!user) route.push("/auth/login");
+        if (routeData.id) {
+            setPost({ description: routeData.description, id: routeData.id });
+        }
+    };
+
+    useEffect(() => {
+        checkUser();
+    }, [user, loading]);
 
     return (
         <div className="my-20 p-12 shadow-lg rounded-lg max-w-md mx-auto">
             <form onSubmit={submitPost}>
-                <h1 className="text-2xl font-bold">Create a new post</h1>
+                <h1 className="text-2xl font-bold">
+                    {post.hasOwnProperty("id") ? "Edit your post" : "Create a new post"}
+                </h1>
                 <div className="py-2">
                     <h3 className="text-lg font-medium py-2">Description</h3>
                     <textarea
                         value={post.description}
                         onChange={(e) => setPost({ ...post, description: e.target.value })}
-                        className="bg-gray-800 h-48 w-full text-white rounded-lg p-2 text-sm">
-                    </textarea>
+                        className="bg-gray-800 h-48 w-full text-white rounded-lg p-2 text-sm"
+                    ></textarea>
                     <p
                         className={`text-cyan-600 font-medium text-sm ${post.description.length > 300 ? "text-red-600" : ""
                             }`}
@@ -39,7 +102,5 @@ function Post() {
                 </button>
             </form>
         </div>
-    )
+    );
 }
-
-export default Post
