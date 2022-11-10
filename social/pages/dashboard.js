7b1de17d5acdff15ea1a38633de23d6980cb6535
Artos Web <teodor.at.tod@gmail.com>
2022-11-10 22:00:42 +0200

    delete and edit post functionallity

--- social/pages/dashboard.js
+++ social/pages/dashboard.js
@@ -1,31 +1,79 @@
-import { auth } from "../utils/firebase";
+import { auth, db } from "../utils/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import {
+    collection,
+    deleteDoc,
+    doc,
+    onSnapshot,
+    query,
+    where,
+} from "firebase/firestore";
+import Message from "../components/message";
+import { BsTrash2Fill } from "react-icons/bs";
+import { AiFillEdit } from "react-icons/ai";
+import Link from "next/link";
 
-function Dashboard() {
+export default function Dashboard() {
     const route = useRouter();
     const [user, loading] = useAuthState(auth);
-    //See if user is logged
+    const [posts, setPosts] = useState([]);
+
     const getData = async () => {
         if (loading) return;
-        if (!user) return route.push('/auth/login');
-    }
+        if (!user) return route.push("/auth/login");
+
+        const collectionRef = collection(db, "posts");
+        const q = query(collectionRef, where("user", "==", user.uid));
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+            setPosts(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        });
+        return unsubscribe;
+    };
+
+    //Delete Post
+    const deletePost = async (id) => {
+        const docRef = doc(db, "posts", id);
+        await deleteDoc(docRef);
+    };
 
     //Get users data
-
     useEffect(() => {
         getData();
-    }, [user, loading])
-
+    }, [user, loading]);
 
     return (
         <div>
             <h1>Your posts</h1>
-            <div>posts</div>
-            <button onClick={() => auth.signOut()}>Sign out</button>
+            <div>
+                {posts.map((post) => {
+                    return (
+                        <Message {...post} key={post.id}>
+                            <div className="flex gap-4">
+                                <button
+                                    onClick={() => deletePost(post.id)}
+                                    className="text-pink-600 flex items-center justify-center gap-2 py-2 text-sm"
+                                >
+                                    <BsTrash2Fill className="text-2xl" /> Delete
+                                </button>
+                                <Link href={{ pathname: "/post", query: post }}>
+                                    <button className="text-teal-600 flex items-center justify-center gap-2 py-2 text-sm">
+                                        <AiFillEdit className="text-2xl" />
+                                        Edit
+                                    </button>
+                                </Link>
+                            </div>
+                        </Message>
+                    );
+                })}
+            </div>
+            <button
+                className="font-medium text-white bg-gray-800 py-2 px-4 my-6"
+                onClick={() => auth.signOut()}
+            >
+                Sign out
+            </button>
         </div>
-    )
+    );
 }
-
-export default Dashboard
